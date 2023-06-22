80ce87500baef08be20f231c288c7051faa080b0
nyxb <contact@nyxb.xyz>
2023-06-22 18:57:47 +0200

    🔥 chore(eslint-config): remove '@next/next/no-html-link-for-pages' rule from configuration 🔧 chore(eslint-config): disable '@typescript-eslint/no-use-before-define' rule from configuration to allow usage of variables before they are defined 🔧 chore(eslint-config): remove 'react' version setting from configuration as it is not needed anymore

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -4,12 +4,6 @@ module.exports = {
       '@nyxb/eslint-config-vue',
    ],
    rules: {
-      '@next/next/no-html-link-for-pages': 'off',
       '@typescript-eslint/no-use-before-define': 'off',
    },
-   settings: {
-      react: {
-         version: '18',
-      }
-   }
 }
