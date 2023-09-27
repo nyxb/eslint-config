221639bff1d4d3a91c4d1a2b33a7cad4ce028b78
nyxb <contact@nyxb.email>
2023-09-27 19:48:39 +0200

    feat!: use flat config

--- packages/eslint-config/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-   extends: [
-      '@nyxb/eslint-config-nextjs',
-      '@nyxb/eslint-config-vue',
-   ],
-   rules: {
-      '@typescript-eslint/no-use-before-define': 'off',
-   },
-}
