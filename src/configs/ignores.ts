7ae6814c9a513ad9d2f5745373903b7c89ee863c
nyxb <contact@nyxb.email>
2023-09-30 07:02:16 +0200

    fix: ignores

--- src/configs/ignores.ts
+++ src/configs/ignores.ts
@@ -1,6 +1,10 @@
-import type { FlatESLintConfigItem } from 'eslint-define-config'
+import type { FlatESLintConfigItem } from '../types'
 import { GLOB_EXCLUDE } from '../globs'
 
-export const ignores: FlatESLintConfigItem[] = [
-  { ignores: GLOB_EXCLUDE },
-]
+export function ignores(): FlatESLintConfigItem[] {
+  return [
+    {
+      ignores: GLOB_EXCLUDE,
+    },
+  ]
+}
