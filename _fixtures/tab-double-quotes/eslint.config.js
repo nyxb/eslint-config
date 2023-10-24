94fa5737762218c088a4bc236b8465a82c2ab3c6
nyxb <contact@nyxb.email>
2023-10-24 08:15:30 +0200

    🔥 remove(_fixtures): delete unused fixture files to clean up the codebase

--- _fixtures/tab-double-quotes/eslint.config.js
+++
@@ -1,9 +0,0 @@
-
-// @eslint-disable
-import antfu from '@antfu/eslint-config'
-
-export default antfu(
-  {"typescript":true,"vue":true,"stylistic":{"indent":"tab","quotes":"double"}},
-  ...[],
-)
-  
--- _fixtures/ts-override/eslint.config.js
+++
@@ -1,9 +0,0 @@
-
-// @eslint-disable
-import antfu from '@antfu/eslint-config'
-
-export default antfu(
-  {"typescript":true},
-  ...[{"rules":{"ts/consistent-type-definitions":["error","type"]}}],
-)
-  
