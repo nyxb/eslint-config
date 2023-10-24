aa71866ac5e967148fd9d3af0fbb4b20c9181681
nyxb <contact@nyxb.email>
2023-10-24 08:36:03 +0200

    🔄 refactor(configs): replace antfu plugins with nyxb plugins for better compatibility ✨ feat(configs): add support for next and react in index.ts for extended functionality

--- src/configs/imports.ts
+++ src/configs/imports.ts
@@ -1,36 +1,36 @@
 import type { ConfigItem, OptionsStylistic } from '../types'
-import { pluginAntfu, pluginImport } from '../plugins'
+import { pluginImport, pluginNyxb } from '../plugins'
 
 export function imports(options: OptionsStylistic = {}): ConfigItem[] {
-  const {
-    stylistic = true,
-  } = options
+   const {
+      stylistic = true,
+   } = options
 
-  return [
-    {
-      name: 'antfu:imports',
-      plugins: {
-        antfu: pluginAntfu,
-        import: pluginImport,
+   return [
+      {
+         name: 'nyxb:imports',
+         plugins: {
+            import: pluginImport,
+            nyxb: pluginNyxb,
+         },
+         rules: {
+            'import/first': 'error',
+            'import/no-duplicates': 'error',
+
+            'import/no-mutable-exports': 'error',
+            'import/no-named-default': 'error',
+            'import/no-self-import': 'error',
+            'import/no-webpack-loader-syntax': 'error',
+            'import/order': 'error',
+            'nyxb/import-dedupe': 'error',
+            'nyxb/no-import-node-modules-by-path': 'error',
+
+            ...stylistic
+               ? {
+                     'import/newline-after-import': ['error', { considerComments: true, count: 1 }],
+                  }
+               : {},
+         },
       },
-      rules: {
-        'antfu/import-dedupe': 'error',
-        'antfu/no-import-node-modules-by-path': 'error',
-
-        'import/first': 'error',
-        'import/no-duplicates': 'error',
-        'import/no-mutable-exports': 'error',
-        'import/no-named-default': 'error',
-        'import/no-self-import': 'error',
-        'import/no-webpack-loader-syntax': 'error',
-        'import/order': 'error',
-
-        ...stylistic
-          ? {
-              'import/newline-after-import': ['error', { considerComments: true, count: 1 }],
-            }
-          : {},
-      },
-    },
-  ]
+   ]
 }
