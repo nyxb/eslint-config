706a7ebee660c768585bee85840442a9b012c0d0
nyxb <contact@nyxb.email>
2023-10-24 22:25:38 +0200

    🔧 refactor: increase eslint severity from 'off' to 'error' for better code quality 🎨 style: adjust indentation in eslint.config.js and utils.ts for better readability

--- eslint.config.js
+++ eslint.config.js
@@ -3,27 +3,27 @@ import styleMigrate from '@stylistic/eslint-plugin-migrate'
 import nyxb from './dist/index.js'
 
 export default nyxb(
-  {
-    vue: true,
-    typescript: true,
-    ignores: [
-      'fixtures',
-      '_fixtures',
-    ],
-  },
-  {
-    files: ['src/**/*.ts'],
-    rules: {
-      'sort-keys/sort-keys-fix': 'error',
-    },
-  },
-  {
-    files: ['src/configs/*.ts'],
-    plugins: {
-      'style-migrate': styleMigrate,
-    },
-    rules: {
-      'style-migrate/migrate': ['error', { namespaceTo: 'style' }],
-    },
-  },
+   {
+      vue: true,
+      typescript: true,
+      ignores: [
+         'fixtures',
+         '_fixtures',
+      ],
+   },
+   {
+      files: ['src/**/*.ts'],
+      rules: {
+         'sort-keys/sort-keys-fix': 'error',
+      },
+   },
+   {
+      files: ['src/configs/*.ts'],
+      plugins: {
+         'style-migrate': styleMigrate,
+      },
+      rules: {
+         'style-migrate/migrate': ['error', { namespaceTo: 'style' }],
+      },
+   },
 )
