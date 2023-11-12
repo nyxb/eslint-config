e7969e6a11b0864768a632c69873474a1fae17c2
nyxb <contact@nyxb.email>
2023-11-12 17:06:34 +0100

    ♻️ refactor(utils.ts): simplify combine function using flat method ✨ feat(utils.ts): add toArray helper function to ensure values are returned as arrays

--- src/utils.ts
+++ src/utils.ts
@@ -4,16 +4,20 @@ import type { ConfigItem } from './types'
  * Combine array and non-array configs into a single array.
  */
 export function combine(...configs: (ConfigItem | ConfigItem[])[]): ConfigItem[] {
-   return configs.flatMap(config => Array.isArray(config) ? config : [config])
+  return configs.flat()
 }
 
 export function renameRules(rules: Record<string, any>, from: string, to: string) {
-   return Object.fromEntries(
-      Object.entries(rules)
-         .map(([key, value]) => {
-            if (key.startsWith(from))
-               return [to + key.slice(from.length), value]
-            return [key, value]
-         }),
-   )
+  return Object.fromEntries(
+    Object.entries(rules)
+      .map(([key, value]) => {
+        if (key.startsWith(from))
+          return [to + key.slice(from.length), value]
+        return [key, value]
+      }),
+  )
+}
+
+export function toArray<T>(value: T | T[]): T[] {
+  return Array.isArray(value) ? value : [value]
 }
