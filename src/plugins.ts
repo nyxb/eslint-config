67789f84594b16c44bd93be02c029a5adfe76942
nyxb <contact@nyxb.email>
2023-12-09 02:36:42 +0100

    🎨 style: Adjust severity of ESLint rules in VSCode settings
    ♻️ refactor: Remove unnecessary newline in plugins.ts
    
    ♻️ refactor: Standardize indentation in utils.ts and cli.spec.ts
    
    🎨 style: Apply consistent indentation in tsup.config.ts

--- src/plugins.ts
+++ src/plugins.ts
@@ -1,6 +1,5 @@
 // eslint-disable-next-line ts/ban-ts-comment
 // @ts-nocheck
-
 export { default as pluginNyxb } from 'eslint-plugin-nyxb'
 export { default as pluginComments } from 'eslint-plugin-eslint-comments'
 export * as pluginImport from 'eslint-plugin-i'
